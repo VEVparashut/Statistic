7ce872b6baf8ccd651f36e6e19d4c53e100aa554
Neto test <89373786148@yandex.ru>
2023-01-23 16:28:56 +0300

    first commit

---
+++ src/main/java/ru/netology/stats/Statistic/services/StatsService.java
@@ -0,0 +1,65 @@
+package ru.netology.stats.Statistic.services;
+
+public class StatsService {
+
+    public int sumSales(int[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+        return sum;
+    }
+
+    public int averageSales(int[] sales) {
+        int average = 0;
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+            average = sum / sales.length;
+        }
+        return average;
+    }
+
+    public int getMinSales(int[] sales) {
+        int minSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minSales]) {
+                minSales = i;
+            }
+        }
+        return minSales + 1;
+    }
+
+    public int getMaxSales(int[] sales) {
+        int maxSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxSales]) {
+                maxSales = i;
+            }
+        }
+        return maxSales + 1;
+    }
+
+    public int minAverageSales(int[] sales) {
+        int countMin = 0;
+        int average = 15;
+        for (int s : sales) {
+            if (s < average) {
+                countMin++;
+            }
+        }
+        return countMin;
+    }
+
+    public int maxAverageSales(int[] sales) {
+        int countMax = 0;
+        int average = 15;
+        for (int s : sales) {
+            if (s > average) {
+                countMax++;
+            }
+        }
+        return countMax;
+    }
+}
+
---
+++ src/test/java/ru/netology/stats/Statistic/services/StatsServiceTest.java
@@ -0,0 +1,79 @@
+package ru.netology.stats.Statistic.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldFindSum() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedSum = 180;
+        int actualSum = service.sumSales(sales);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void shouldFindAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverage = 15;
+        int actualAverage = service.averageSales(sales);
+
+        Assertions.assertEquals(expectedAverage, actualAverage);
+    }
+
+    @Test
+    public void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMinMonth = 9;
+        int actualMinMonth = service.getMinSales(sales);
+
+        Assertions.assertEquals(expectedMinMonth, actualMinMonth);
+    }
+
+    @Test
+    public void shouldFindMaxMonth() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMaxMonth = 8;
+        int actualMaxMonth = service.getMaxSales(sales);
+
+        Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
+    }
+
+    @Test
+    public void shouldFindCountMonthMinAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedCountMin = 5;
+        int actualCountMin = service.minAverageSales(sales);
+
+        Assertions.assertEquals(expectedCountMin, actualCountMin);
+    }
+
+    @Test
+    public void shouldFindCountMonthMaxAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedCountMax = 5;
+        int actualCountMax = service.maxAverageSales(sales);
+
+        Assertions.assertEquals(expectedCountMax, actualCountMax);
+    }
+}
